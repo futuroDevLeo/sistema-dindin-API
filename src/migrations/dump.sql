070c0ca4bd89acd59a8789a63e401679074f987d
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-10-05 20:11:54 -0300

    Revert "Maxson"

--- src/migrations/dump.sql
+++ src/migrations/dump.sql
@@ -1,18 +1,18 @@
 create database dindin;
 
-create table if not exists usuarios (
+create table usuarios (
   id serial primary key,
   nome text not null,
   email text unique not null,
   senha text not null
  );
 
-create table if not exists categorias (
+create table categorias (
   id serial primary key,
   descricao text not null
  );
   
-create table if not exists transacoes (
+create table transacoes (
   id serial primary key,
   descricao text not null,
   valor integer not null,
