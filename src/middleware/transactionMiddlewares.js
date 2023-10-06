44f17d7ba4e23302d3d52e705b546dca9b1a46be
Maxson Ferovante <maxsonferovante@users.noreply.github.com>
2023-10-06 17:47:43 -0300

    add: validação para criaçãode transações

--- src/middleware/transactionMiddlewares.js
+++ src/middleware/transactionMiddlewares.js
@@ -3,6 +3,19 @@ const { body, param } = require('express-validator');
 const { validationMiddleware } = require('./validationMiddleware');
 
 
+const validateCreateTransaction = [
+    body('descricao').notEmpty().withMessage('Descrição é obrigatória'),
+    body('valor').notEmpty().withMessage('Valor é obrigatório'),
+    body('data').notEmpty().withMessage('Data é obrigatória'),
+    body('categoria_id').notEmpty().withMessage('Categoria é obrigatória'),
+    body('tipo').notEmpty().withMessage('Tipo é obrigatório').custom(value => {
+        if (value !== 'entrada' && value !== 'saida') {
+            throw new Error('Tipo deve ser "entrada" ou "saida"');
+        }
+        return true;
+    }),
+    validationMiddleware,
+];
 const validateUpdateTransaction = [
     param('id').notEmpty().withMessage('Id da transação é obrigatório.'),
     body('descricao').notEmpty().withMessage('Descrição é obrigatória'),
@@ -21,4 +34,5 @@ const validateUpdateTransaction = [
 
 module.exports = {
     validateUpdateTransaction,
+    validateCreateTransaction,
 };
